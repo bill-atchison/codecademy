21f76e658dfafcaba8055d2f4073ec46b9d0cace
William Atchison <billa.spacebook@gmail.com>
2023-05-19 22:37:09 -0400

    Community News Forum

---
+++ Java/CommunityNewsForum/src/main/java/com/codecademy/viewmodels/MainThread.java
@@ -0,0 +1,9 @@
+package com.codecademy.viewmodels;
+
+public class MainThread {
+
+    public static void main(String[] args) {
+        CommunityNewsForum newsForum = new CommunityNewsForum();
+        newsForum.start();
+    }
+}
